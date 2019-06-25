204ac826e486c371c8a205858b6f5f0f11d1d725
Alejandro Tejada <alejandrotejada@quibim.com>
2019-06-25 23:14:13 +0200

    health bar implemented

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -13,35 +13,40 @@ export class CharacterComponent implements OnInit {
       healthPoints: 67,
       strenght: 49,
       intelect: 65,
-      avatar: '../../assets/img/zulrak.jpg'
+      avatar: '../../assets/img/zulrak.jpg',
+      currentHealth: 43
     },
     {
       name: 'Kundox',
       healthPoints: 90,
       strenght: 78,
       intelect: 65,
-      avatar: '../../assets/img/kundox.jpg'
+      avatar: '../../assets/img/kundox.jpg',
+      currentHealth: 32
     },
     {
       name: 'Kennedian',
       healthPoints: 43,
       strenght: 54,
       intelect: 65,
-      avatar: '../../assets/img/outlander.jpg'
+      avatar: '../../assets/img/outlander.jpg',
+      currentHealth: 37
     },
     {
       name: 'Fanrisa',
-      healthPoints: 2,
+      healthPoints: 56,
       strenght: 34,
       intelect: 91,
-      avatar: '../../assets/img/mage.jpg'
+      avatar: '../../assets/img/mage.jpg',
+      currentHealth: 25
     },
     {
       name: 'Hawkrager',
       healthPoints: 56,
       strenght: 46,
       intelect: 75,
-      avatar: '../../assets/img/berserker.jpg'
+      avatar: '../../assets/img/berserker.jpg',
+      currentHealth: 12
     }
   ];
 
